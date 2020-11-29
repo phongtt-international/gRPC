6adfe169de1bd1609837a0a347654c25e4729f47
phongtt <phongtt.international@gmail.com>
2020-11-29 12:18:08 +0700

    Refactoring code C#9

--- GrpcMicroservices/ProductGrpcClient/Program.cs
+++ GrpcMicroservices/ProductGrpcClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using Grpc.Net.Client;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
 using ProductGrpc.Protos;
 using System;
 using System.Threading;
@@ -16,6 +17,24 @@ namespace ProductGrpcClient
             using var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client = new ProductProtoService.ProductProtoServiceClient(channel);
 
+            await GetProductAsync(client);
+            await GetAllProducts(client);
+        }
+
+        private static async Task GetAllProducts(ProductProtoService.ProductProtoServiceClient client)
+        {
+            //GetAllProductAsync with C#9
+            Console.WriteLine("GetAllProductAsync with C#9 started...");
+            using var clientData = client.GetAllProducts(new GetAllProductsRequest());
+            await foreach (var responseData in clientData.ResponseStream.ReadAllAsync())
+            {
+                Console.WriteLine(responseData);
+            }
+            Console.ReadLine();
+        }
+
+        private static async Task GetProductAsync(ProductProtoService.ProductProtoServiceClient client)
+        {
             //GetProductAsync
             Console.WriteLine("GetProductAsync started...");
             var response = await client.GetProductAsync(
@@ -24,17 +43,6 @@ namespace ProductGrpcClient
                     ProductId = 1
                 });
             Console.WriteLine("GetProductAsync response: " + response.ToString());
-            //GetAllProductAsync
-            Console.WriteLine("GetAllProductAsync started...");
-            using (var clientData = client.GetAllProducts(new GetAllProductsRequest()))
-            {
-                while (await clientData.ResponseStream.MoveNext(new System.Threading.CancellationToken()))
-                {
-                    var currentProduct = clientData.ResponseStream.Current;
-                    Console.WriteLine(currentProduct);
-                }
-            }
-            Console.ReadLine();
         }
     }
 }

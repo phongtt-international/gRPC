ec80bda27310998f07936392842842a9ab21f3ad
phongtt <phongtt.international@gmail.com>
2020-11-29 11:34:47 +0700

    Implement gRPC Consume Product

---
+++ GrpcMicroservices/ProductGrpcClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Net.Client;
+using ProductGrpc.Protos;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProductGrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Waitting for server is running....");
+            Thread.Sleep(2000);
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new ProductProtoService.ProductProtoServiceClient(channel);
+
+            //GetProductAsync
+            Console.WriteLine("GetProductAsync started...");
+            var response = await client.GetProductAsync(
+                new GetProductRequest
+                {
+                    ProductId = 1
+                });
+            Console.WriteLine("GetProductAsync response: " + response.ToString());
+            Console.ReadLine();
+        }
+    }
+}
